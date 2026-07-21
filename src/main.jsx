9e11dfda418b6e9d3c9da29a89a862af5f79320f
Vaibhav <vaibhav2006tygai@gmail.com>
2026-07-22 02:49:14 +0530

    add_leftNavigationPanel_basic_Data

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import ReactDOM from 'react-dom/client'
+import LeftPanel from 'C:/Users/Vaibhav/OneDrive/Desktop/React/DevBoard/components/LeftNavigation.jsx'
+
+
+const root= ReactDOM.createRoot( document.querySelector('.entireProject') );
+
+root.render(
+    <LeftPanel/>
+)
+;
